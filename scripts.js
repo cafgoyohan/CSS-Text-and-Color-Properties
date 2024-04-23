667f76f700405cb7af100823f9b3f3feb3f0e7ef
Goyo <edrickdevilla.me@gmail.com>
2024-04-24 02:59:17 +0800

    mostly done

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function openPopup(popupId) {
+    var popup = document.getElementById(popupId);
+    if (popup) {
+        popup.style.display = "block";
+    }
+}
+
+function closePopup() {
+    var popups = document.getElementsByClassName("popup");
+    for (var i = 0; i < popups.length; i++) {
+        popups[i].style.display = "none";
+    }
+}
